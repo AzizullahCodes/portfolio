38d26cf46cde3b1c8b56a9d6185f624b475f44d4
Azizullah <azizullahcodes@gmail.com>
2026-01-29 19:43:51 +0500

    upd

--- portfolio/src/assets/navbar/navbar.jsx
+++ portfolio/src/assets/navbar/navbar.jsx
@@ -7,7 +7,7 @@ import { NavLink } from 'react-router-dom';
 function MyNavbar() {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
-      <Container>
+      <Container fluid> {/* Full width */}
         <Navbar.Brand as={NavLink} to="/">
           Portfolio
         </Navbar.Brand>
@@ -16,35 +16,13 @@ function MyNavbar() {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/">
-              Home
-            </Nav.Link>
-
-            <Nav.Link as={NavLink} to="/about">
-              About
-            </Nav.Link>
-
-            <Nav.Link as={NavLink} to="/services">
-              Services
-            </Nav.Link>
-
-             <Nav.Link as={NavLink} to="/cssProjects">
-              Css-Projects
-            </Nav.Link>
-
-             <Nav.Link as={NavLink} to="/jsTasks">
-              JsTasks
-            </Nav.Link>
-
-             <Nav.Link as={NavLink} to="codingExercises">
-              Coding-Exercises
-            </Nav.Link>
-
-             <Nav.Link as={NavLink} to="/contact">
-              Contact
-            </Nav.Link>
-
-
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+            <Nav.Link as={NavLink} to="/services">Services</Nav.Link>
+            <Nav.Link as={NavLink} to="/cssProjects">Css-Projects</Nav.Link>
+            <Nav.Link as={NavLink} to="/jsTasks">JsTasks</Nav.Link>
+            <Nav.Link as={NavLink} to="/codingExercises">Coding-Exercises</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- portfolio/src/assets/pages/home.jsx
+++ portfolio/src/assets/pages/home.jsx
@@ -1,10 +1,43 @@
 // home.jsx
 import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope, FaWhatsapp } from "react-icons/fa";
+import myimage from '../../assets//profileImages/myimage.png';
+import '../cssFiles/home.css'
 const Home = ()=>{
     return(
-        <>
-        <h1>home page</h1>
-        </>
+       <section className="home">
+        <div className="homeContainer">
+            {/* left-column */}
+           <div className="home-left"> <h1>Hello, I'm Azizullah
+Creative UI/UX Designer|</h1>
+
+<p>I’m a Frontend Website Developer passionate about creating clean, responsive, and user-friendly web experiences. I love turning ideas into interactive, modern, and visually appealing websites using HTML, CSS, JavaScript, and modern frameworks.</p>
+
+<div className="buttondiv">
+    <button>View My Work</button>
+    <button>Get In Touch</button>
+</div>
+<div className="home-icon">
+ <a href="https://github.com/" target="_blank" rel="noreferrer">
+              <FaGithub />
+            </a>
+            <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
+              <FaLinkedin />
+            </a>
+            <a href="mailto:example@email.com">
+              <FaEnvelope />
+            </a>
+            <a href="https://wa.me/923000000000" target="_blank" rel="noreferrer">
+              <FaWhatsapp />
+            </a>
+</div></div>
+            {/* right-column */}
+            <div className="home-right">
+                <img src={myimage} alt="" />
+            </div>
+        </div>
+
+       </section>
     )
 }
 export default Home;
