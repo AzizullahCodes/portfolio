0f29302927cf0883449a733bcec04e0b9c42624d
Azizullah <azizullahcodes@gmail.com>
2026-01-31 17:10:32 +0500

    services is added

--- portfolio/src/assets/pages/sevices.jsx
+++ portfolio/src/assets/pages/sevices.jsx
@@ -1,1 +1,36 @@
+// about.jsx
+import React from "react";
+import '../cssFiles/services.css';
+const About = ()=>{
+    return(
+       <section className="about">
+       <div className="aboutContainer">
+        <div className="firstRow">
+<h1>About</h1>
+<p>I am a Frontend Web Developer with a strong understanding of HTML, CSS, Bootstrap, JavaScript, and React.js. I have built multiple CSS and JavaScript projects for practice during my learning journey and continue to improve my skills through real projects.</p>
 
+        </div>
+        
+        {/* =========== fourthRow ======== */}
+        <div className="fourthRow">
+            {/* ======= card html ===== */}
+          <div className="card">
+            <h3>HTML</h3>
+            <p>Skilled in creating well-structured, semantic web pages with clean and organized code. Proficient in building responsive layouts and using HTML5 best practices.</p>
+           
+          </div>
+            {/* ======= card css ===== */}
+          <div className="card">
+            <h3>CSS</h3>
+            <p>Able to design visually appealing and responsive web pages with modern styling techniques. Skilled in CSS3 features, layouts, and customizing designs for different devices.</p>
+           
+          </div>
+           
+           
+          
+        </div>
+       </div>
+       </section>
+    )
+}
+export default About;
