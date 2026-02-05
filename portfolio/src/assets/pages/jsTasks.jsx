314decc9fc0f805689de25d415c1c54689870896
Azizullah <azizullahcodes@gmail.com>
2026-02-05 09:42:55 +0500

    upd

--- portfolio/src/assets/pages/jsTasks.jsx
+++ portfolio/src/assets/pages/jsTasks.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const JsTasks = ()=>{
     return(
         <>
-        <h1>JsTasks page is not working</h1>
+        <h1>JsTasks page is not working p</h1>
         </>
     )
 }
